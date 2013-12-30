a8fd62d2802b18f5358a783765a55393764ae06b
Daniel Loureiro <loureirorg@gmail.com>
2013-12-29 23:42:59 -0200

    first version

--- lib/piet-binary.rb
+++ lib/piet-binary.rb
@@ -6,17 +6,35 @@ module Piet
 
     private
 
+    def which(cmd)
+      exts = ENV['PATHEXT'] ? ENV['PATHEXT'].split(';') : ['']
+      ENV['PATH'].split(File::PATH_SEPARATOR).each do |path|
+        exts.each do |ext|
+          exe = File.join(path, "#{cmd}#{ext}")
+          return exe if File.executable? exe
+        end
+      end
+      return nil
+    end
+
     def command_path(command)
-      folder = if OS.osx?
-        'osx'
-      elsif OS.linux?
-        File.join('linux', OS.bits.eql?(64) ? 'x64' : 'x86')
-      elsif OS.windows?
-        OS.bits.eql?(64) ? 'win64' : 'win32'
+      path = which(command)
+
+      if path.blank?
+
+        folder = if OS.osx?
+          'osx'
+        elsif OS.linux?
+          File.join('linux', OS.bits.eql?(64) ? 'x64' : 'x86')
+        elsif OS.windows?
+          OS.bits.eql?(64) ? 'win64' : 'win32'
+        end
+
+        root = Gem::Specification.find_by_name('piet-binary').gem_dir
+        path = File.join(root, 'bin', folder, command)
       end
 
-      root = Gem::Specification.find_by_name('piet-binary').gem_dir
-      File.join(root, 'bin', folder, command)
+      return path
     end
 
   end
